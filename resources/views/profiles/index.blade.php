bd29bd4ec87ff2ebdee878b5cbfd5e8e835b4d1b
wahyono17 <wahyono17@gmail.com>
2019-10-11 11:32:41 +0700

    before merge

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -2,8 +2,9 @@
 @section('content')
 
 <div class="container">
-    <div class="row-col-md-3">
-        <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fkimbosf.files.wordpress.com%2F2013%2F10%2Fnot-available.jpg&imgrefurl=https%3A%2F%2Finwritefield.com%2F2014%2F05%2F08%2Fkeep-me-in-the-picture%2F&docid=oEwAiLPp3lTMeM&tbnid=45NGd1tOjjlxkM%3A&vet=10ahUKEwirpNGj0pDlAhXLKY8KHc5fDq4QMwiMASgvMC8..i&w=190&h=250&safe=strict&bih=709&biw=1429&q=not%20available%20picture&ved=0ahUKEwirpNGj0pDlAhXLKY8KHc5fDq4QMwiMASgvMC8&iact=mrc&uact=8">
+    <div class="row-col-md-3 ">
+        <img src="" class="w-100">
     </div>
+    
 </div>
 @endsection
