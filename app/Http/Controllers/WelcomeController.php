6110bc9c3e8834bee047212ef197952b15197fd5
wahyono17 <wahyono17@gmail.com>
2019-10-14 18:02:24 +0700

    welcome

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,13 +1,13 @@
 <?php
-use App\Post;
 namespace App\Http\Controllers;
-
+use App\Post;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function index(){
-        $posts = Post::all()->latest()->paginate(10);
+        $posts = Post::orderBy('updated_at', 'DESC')->get();
+        //dd($posts);
         return view('welcome', compact('posts'));
     }
 }
